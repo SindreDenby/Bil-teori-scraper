69a7ca5977510393dbd3c375d6de8c3fbfdbf76a
Sindre Denby <sidenby1@gmail.com>
2023-01-04 20:04:51 +0100

    qwewasdasdw

--- cleaner.py
+++ cleaner.py
@@ -1,18 +1,23 @@
 import json
 
-def sort_questions(questions: list[object]):
+def sort_questions(questions: list[dict[str, str]]):
     return sorted(questions, key=(lambda x: x['title'])) # type: ignore
 
-def remove_duplicates(questions: list[object]):
-    return list(set(questions))
+def remove_duplicates(questions: list[dict[str, str]]):
+    res_list: list[dict[str, str]] = []
+    for i in range(len(questions)):
+        if questions[i] not in questions[i + 1:]:
+            res_list.append(questions[i])
+
+    return res_list
 
 def main():
     with open('out.json', 'r') as f:
-        questions = json.load(f)
-    print(questions)
+        questions: list[dict[str, str]] = json.load(f)
+
     questions = remove_duplicates(questions)
 
-    
+    questions = sort_questions(questions)
     with open('sorted.json', 'w+') as f:
         f.write(json.dumps(questions))
     
