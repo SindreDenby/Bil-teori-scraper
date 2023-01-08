affcd62edba77a4d30e8dbd1142e30e0e30a311a
Sindre Denby <sidenby1@gmail.com>
2023-01-08 16:00:33 +0100

    stripped html

---
+++ html_stripper.py
@@ -0,0 +1,33 @@
+from io import StringIO
+from html.parser import HTMLParser
+import json
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d): # type: ignore
+        self.text.write(d)  # type: ignore
+    def get_data(self):
+        return self.text.getvalue()
+
+def strip_tags(html: str):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def main():
+    with open('out.json', 'r') as f:
+        questions: list[dict[str, str]] = json.load(f)
+
+    for i, question in enumerate(questions) :
+        questions[i]['title'] = strip_tags(question['title'])
+    
+    with open('stripped.json', 'w+') as f:
+        f.write(json.dumps(questions, indent=4))
+
+if __name__ == '__main__':
+    main()
