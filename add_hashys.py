ddfdee9a2d8e24c5f654ab60b6637b9548c24d8d
Sindre Denby <sidenby1@gmail.com>
2023-01-08 16:55:58 +0100

    -

---
+++ add_hashys.py
@@ -0,0 +1,16 @@
+import json
+import random
+
+
+def main():
+    with open('stripped.json', 'r') as f:
+        questions = json.load(f)
+
+    for i, question in enumerate(questions):
+        questions[i]['id'] = random.getrandbits(128)
+    
+    with open('hashed.json', 'w+') as f:
+        f.write(json.dumps(questions, indent=4))
+
+if __name__ == '__main__':
+    main()
