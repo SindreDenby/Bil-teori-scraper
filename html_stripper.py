61b9a6f8f6c90d10c61ff8360dc5de1978620748
Sindre Denby <sidenby1@gmail.com>
2023-01-08 16:03:53 +0100

    -

--- html_stripper.py
+++ html_stripper.py
@@ -19,15 +19,18 @@ def strip_tags(html: str):
     s.feed(html)
     return s.get_data()
 
-def main():
+def strip_all_html_from_questions():
     with open('out.json', 'r') as f:
         questions: list[dict[str, str]] = json.load(f)
 
     for i, question in enumerate(questions) :
-        questions[i]['title'] = strip_tags(question['title'])
+        questions[i]['title'] = strip_tags(question['title']).replace("\n", "")
     
     with open('stripped.json', 'w+') as f:
         f.write(json.dumps(questions, indent=4))
+
+def main():
+    strip_all_html_from_questions()
 
 if __name__ == '__main__':
     main()
